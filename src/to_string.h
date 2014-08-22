3eefebe465f5902aeb363c48d3a736fc6ad2041f
Lutz <lutz.reinhardt@tu-bs.de>
2014-08-22 15:07:29 +0200

    allow container to be const for get_c_string_array

--- src/to_string.h
+++ src/to_string.h
@@ -17,10 +17,10 @@ bool contains_only_valid_characters(const std::string& input, const std::string&
 std::string test_characters(const std::string& input, const std::string& valid_chars, std::string error_message);
 
 template<typename Container>
-std::vector<const char *> get_c_string_array(Container& strings) {
+std::vector<const char *> get_c_string_array(const Container& strings) {
         static_assert(std::is_same<typename std::decay<Container>::type::value_type, std::string>::value, "container has to carry std::string");
         std::vector<const char *> ch_ptr;
-        std::transform(std::begin(strings), std::end(strings), std::back_inserter(ch_ptr), [](std::string& s){return s.c_str();});
+        std::transform(std::begin(strings), std::end(strings), std::back_inserter(ch_ptr), [](const std::string& s){return s.c_str();});
         // null termination
         ch_ptr.push_back(nullptr);
         return ch_ptr;
