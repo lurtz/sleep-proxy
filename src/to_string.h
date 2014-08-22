bcd702bebda3885dcfe2c4c5fe72636fc989d5a1
Lutz <lutz.reinhardt@tu-bs.de>
2014-08-22 14:44:20 +0200

    make to_c_string_array really container independent

--- src/to_string.h
+++ src/to_string.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 template<typename T>
 std::string to_string(T&& t) {
@@ -15,5 +16,13 @@ bool contains_only_valid_characters(const std::string& input, const std::string&
 
 std::string test_characters(const std::string& input, const std::string& valid_chars, std::string error_message);
 
-std::vector<const char *> get_c_string_array(std::vector<std::string>& strings);
+template<typename Container>
+std::vector<const char *> get_c_string_array(Container& strings) {
+        static_assert(std::is_same<typename std::decay<Container>::type::value_type, std::string>::value, "container has to carry std::string");
+        std::vector<const char *> ch_ptr;
+        std::transform(std::begin(strings), std::end(strings), std::back_inserter(ch_ptr), [](std::string& s){return s.c_str();});
+        // null termination
+        ch_ptr.push_back(nullptr);
+        return ch_ptr;
+}
 
