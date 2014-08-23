017b33b90fcd6813065dd0493e8b2f3ffc637468
Lutz <lutz.reinhardt@tu-bs.de>
2014-08-23 21:26:06 +0200

    wol_ethernet() needs to be updated to a more modern approach

--- src/wol.h
+++ src/wol.h
@@ -3,5 +3,6 @@
 #include <string>
 
 void wol_udp(const std::string& mac);
+// TODO from dmesg: [10748.090978] emulateHost uses obsolete (PF_INET,SOCK_PACKET)
 void wol_ethernet(const std::string& iface, const std::string& mac);
 
