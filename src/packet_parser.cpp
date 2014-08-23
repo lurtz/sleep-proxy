1cc68405728ccd6f720d10ff7a79c6bb8f75c61e
Lutz <lutz.reinhardt@tu-bs.de>
2014-08-23 21:25:51 +0200

    skip vlan header

--- src/packet_parser.cpp
+++ src/packet_parser.cpp
@@ -28,10 +28,18 @@ basic_headers get_headers(const int type, const std::vector<u_char>& packet) {
         }
         data += static_cast<std::vector<u_char>::const_iterator::difference_type>(ll->header_length());
 
+        // possible VLAN header, skip it
+        uint16_t payload_type = ll->payload_protocol();
+        if (payload_type == VLAN_HEADER) {
+                std::unique_ptr<Link_layer> vlan_header = parse_link_layer(payload_type, data, end);
+                payload_type = vlan_header->payload_protocol();
+                data += static_cast<std::vector<u_char>::const_iterator::difference_type>(vlan_header->header_length());
+        }
+
         // IP header
-        std::unique_ptr<ip> ipp = parse_ip(ll->payload_protocol(), data, end);
+        std::unique_ptr<ip> ipp = parse_ip(payload_type, data, end);
         if (ipp == nullptr) {
-                std::cerr << "unsupported link layer payload: " << static_cast<unsigned int>(ll->payload_protocol()) << std::endl;
+                std::cerr << "unsupported link layer payload: " << static_cast<unsigned int>(payload_type) << std::endl;
                 return std::make_tuple(std::move(ll), std::unique_ptr<ip>(nullptr), std::unique_ptr<tp>(nullptr));
         }
         data += static_cast<std::vector<u_char>::const_iterator::difference_type>(ipp->header_length());
