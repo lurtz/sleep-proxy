f635d18f3284eb39a8dc8b03dd1ac3a0529d0ac1
Lutz <lutz.reinhardt@tu-bs.de>
2014-09-06 00:27:49 +0200

    gplv2 or later for everyone

--- src/wol.h
+++ src/wol.h
@@ -1,9 +1,23 @@
+// Copyright (C) 2014  Lutz Reinhardt
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 #pragma once
 
 #include <string>
 
 void wol_udp(const std::string& mac);
-// TODO from dmesg: [10748.090978] emulateHost uses obsolete (PF_INET,SOCK_PACKET)
-void wol_ethernet_pcap(const std::string& iface, const std::string& mac);
 void wol_ethernet(const std::string& iface, const std::string& mac);
 
