07c5e5ee673d3e434e7749dcf2ba7eac505ffe5a
Lutz <lutz.reinhardt@tu-bs.de>
2014-09-06 17:20:35 +0200

    copy tests configs to the test binaries

--- tests/args_test.cpp
+++ tests/args_test.cpp
@@ -168,7 +168,7 @@ class Args_test : public CppUnit::TestFixture {
         }
 
         void test_read_file() {
-                auto args = get_args("../watchhosts");
+                auto args = get_args("watchhosts");
                 CPPUNIT_ASSERT_EQUAL(static_cast<unsigned long>(2), args.size());
                 interface = "lo";
                 addresses = "10.0.0.1/16,fe80::123/64";
@@ -186,10 +186,10 @@ class Args_test : public CppUnit::TestFixture {
                 ping_tries = "1";
                 compare(args.at(1));
 
-                auto args2 = get_args("../watchhosts-empty");
+                auto args2 = get_args("watchhosts-empty");
                 CPPUNIT_ASSERT_EQUAL(static_cast<unsigned long>(0), args2.size());
 
-                auto args3 = get_args("../watchhosts", true);
+                auto args3 = get_args("watchhosts", true);
                 CPPUNIT_ASSERT_EQUAL(true, args3.at(0).syslog);
                 CPPUNIT_ASSERT_EQUAL(true, args3.at(1).syslog);
         }
