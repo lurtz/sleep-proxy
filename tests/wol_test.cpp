e779cfc7dbdda799377f9e704b17f6d91dd5d87d
Lutz <derlurtz@gmail.com>
2014-09-25 19:23:30 +0200

    test creation of wol udp payload

---
+++ tests/wol_test.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2014  Lutz Reinhardt
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#include "main.h"
+#include <string>
+#include "check_range.h"
+
+std::vector<uint8_t> create_wol_udp_payload(const std::string& mac);
+
+class Wol_test : public CppUnit::TestFixture {
+        CPPUNIT_TEST_SUITE( Wol_test );
+        CPPUNIT_TEST( test_create_wol_udp_payload );
+        CPPUNIT_TEST_SUITE_END();
+        public:
+        void setUp() {}
+        void tearDown() {}
+
+        static void check_wol_udp_payload(const std::vector<uint8_t>& wol, const unsigned char start, const unsigned char end_pos) {
+                auto data = std::begin(wol);
+                const auto end = std::begin(wol);
+                for (unsigned int i = 0; i < 6 && data < end; i++, data++) {
+                        CPPUNIT_ASSERT_EQUAL(static_cast<uint8_t>(255), *data);
+                }
+                for (unsigned int i = 0; i < 20; i++) {
+                        check_range(data, end, start, end_pos);
+                }
+                CPPUNIT_ASSERT(data == end);
+        }
+
+        void test_create_wol_udp_payload() {
+                auto wol_packet = create_wol_udp_payload("11:22:33:44:55:66");
+                check_wol_udp_payload(wol_packet, 1, 7);
+                wol_packet = create_wol_udp_payload("88:99:aA:bB:cc:dd");
+                check_wol_udp_payload(wol_packet, 8, 14);
+        }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( Wol_test );
+
