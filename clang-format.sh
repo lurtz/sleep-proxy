734b0d0cd1d9d589e940fc364e5d1266944a6ed6
Lutz <lutz@example.com>
2023-02-13 09:24:44 +0100

    fix build

--- clang-format.sh
+++ clang-format.sh
@@ -2,4 +2,13 @@
 
 find * -name "*.h" -or -name "*.cpp" | xargs clang-format -style file -i
 
+MSG="The following files have been modified:"
+dirty=$(git ls-files --modified)
+
+if [[ $dirty ]]; then
+	echo $MSG
+	echo $dirty
+	exit 1
+fi
+
 exit 0
